a255a19312ebaa2eaabf7179b0f8b299f03adff7
Adnan Ashraf <af.adnan23@gmail.com>
2024-09-27 00:19:10 +0530

    Added Notification msg useing toast

--- cloudinary-saas/app/(app)/video-upload/page.tsx
+++ cloudinary-saas/app/(app)/video-upload/page.tsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import axios from 'axios'
 import {useRouter} from 'next/navigation'
+import { toast } from 'react-toastify';
 
 const VideoUpload = () => {
 const [file,setFile] = useState<File | null>(null)
@@ -12,6 +13,20 @@ const route = useRouter()
 //max file size of 100mb
 
 const MAX_FILE_SIZE = 100 * 1024 *1024
+
+const handleSubmit = async (e:React.FormEvent) => {
+  e.preventDefault()
+  if(!file) return ;
+
+  if (file.size > MAX_FILE_SIZE) {
+    //add notification
+    toast.error('File size exceeds the maximum allowed size of 100MB.');
+    return;
+  }
+
+}
+
+
 
   return (
     <div>VideoUpload</div>
