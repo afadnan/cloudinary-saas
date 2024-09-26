9e32e9ca62c4cb966601f975d50ad5210415dec6
Adnan Ashraf <af.adnan23@gmail.com>
2024-09-27 00:07:48 +0530

    Maximum fileSize is 100mb

--- cloudinary-saas/app/(app)/video-upload/page.tsx
+++ cloudinary-saas/app/(app)/video-upload/page.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios'
+import {useRouter} from 'next/navigation'
 
 const VideoUpload = () => {
+const [file,setFile] = useState<File | null>(null)
+const [title,setTitle] = useState("");
+const [description,setDescription] = useState("");
+const [isUploading,setIsUploading] = useState(false)
+
+const route = useRouter()
+
+
   return (
     <div>VideoUpload</div>
   )
