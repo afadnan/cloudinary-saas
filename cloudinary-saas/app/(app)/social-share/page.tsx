4f50067583941a892a30e725ec1127880fb5c6e8
Adnan Ashraf <af.adnan23@gmail.com>
2024-09-26 18:30:17 +0530

    SocialFormats values

--- cloudinary-saas/app/(app)/social-share/page.tsx
+++ cloudinary-saas/app/(app)/social-share/page.tsx
@@ -3,7 +3,7 @@
 import React,{useState,useEffect,useRef} from 'react';
 import {CldImage} from 'next-cloudinary';
 
-const solcialFormats = {
+const socialFormats = {
   "Instagram Square (1:1)": {width:1080 ,height:1080 ,aspectRatio:"1:1"},
   "Instagram Protrait (4:5)": {width:1080 ,height:1350 ,aspectRatio:"4:5"},
   "Twitter Post (16:9)": {width:1200 ,height:675 ,aspectRatio:"16:19"},
@@ -11,7 +11,7 @@ const solcialFormats = {
   "Facebook Cover (205:78)": {width:820 ,height:312 ,aspectRatio:"205:78"},
 };
 
-type SocialFormat = keyof typeof solcialFormats;
+type SocialFormat = keyof typeof socialFormats;
 
 export default function SocialShare () {
   return (
