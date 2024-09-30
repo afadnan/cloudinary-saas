0d63077441c811b1e34f5d4e397b7b37cc4a6781
Adnan Ashraf <af.adnan23@gmail.com>
2024-10-01 01:39:16 +0530

    getTheVideos rerenderWithDependency

--- cloudinary-saas/app/(app)/home/page.tsx
+++ cloudinary-saas/app/(app)/home/page.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+"use client"
 
+import React,{useState,useEffect,useCallback} from 'react'
+import axios from 'axios'
+import VideoCard from "@/components/VideoCard"
+import {Video} from "@/types"
 const Home = () => {
+  const [videos,setVideos] = useState<Video[]>([])
+  const [loading,setLoading] = useState(true)
+  const [error , setError] = useState <string | null>(null)
+
+
+    //getthevideos
+    const fetchVideos = useCallback(async() => {
+      try {
+       const response = await  axios.get("/api/videos")
+       if(Array.isArray(response.data)) {
+        setVideos(response.data)
+       } else {
+        throw new Error ("Unexpected response format")
+       }
+      } catch (error) {
+        console.log(error);
+        setError("Failed to fetch videos")
+        
+      }
+    },[]) 
+
   return (
     <div>Home</div>
   )
